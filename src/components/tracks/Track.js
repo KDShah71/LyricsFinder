5598aece799585c2a53f0efe33c4a014cc54a69a
Kundan Shah <kundanshah71@gmail.com>
2019-05-22 22:52:01 +0545

    api from musixmatch used to in tracks list and track components

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Track = props => {
+  return (
+    <div className=" col s12 m12 xl6 ">
+      <div className="card z-depth-1">
+        <div className="card-content">
+          <h5>{props.track.track_name}</h5>
+
+          <p>
+            <strong>
+              <i className="fas fa-microphone" /> Artist
+            </strong>
+            : {props.track.artist_name}
+          </p>
+          <p>
+            <strong>
+              <i className="fas fa-compact-disc" /> Album
+            </strong>
+            : {props.track.album_name}
+          </p>
+
+          <Link
+            to={`lyrics/track/${props.track.track_id}`}
+            className="btn-flat btn-small "
+          >
+            <i className="fas fa-chevron-right "> </i>View Lyrics
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Track;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
 
+import Spinner from "../layout/Spinner";
+import Track from "./Track";
+
 class Tracks extends Component {
   render() {
     return (
-     <Consumer>
-       {
-         value => {
-           console.log(value);
-           return <h1>Hey world</h1>
-         }
-       }
-     </Consumer>
+      <Consumer>
+        {value => {
+          console.log(value);
+
+          const { track_list } = value;
+
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner />;
+          } else {
+            return (
+              <React.Fragment>
+                <h3 className="center">{value.heading}</h3>
+                <div className="row">
+                  {value.track_list.map(item => (
+                    <Track
+                      key={item.track.track_id}
+                      track={item.track}
+                    />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          }
+        }}
+      </Consumer>
     );
   }
 }
